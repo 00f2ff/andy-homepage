8e58a5c92b168425e5141cb74c78df830f6378c3
Duncan McIsaac <dbm@andrew.cmu.edu>
2015-04-24 03:04:16 -0400

    Decreased initial current_scene so code is easier to understand

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(function() {
 
-	/* THIS IS NOT CONSISTENT */
+	/* THIS IS NOT CONSISTENT
+	 * I may want to move this up a file along with the scroll tracking stuff
+	 */
 	// This sets margins for #scene1 children
 	$('#scene1 .small').css('margin-left', $('#scene1 .big').width() - $('#scene1 .small').width() / 4 + 'px');
 	$('#scene1 .medium').css('margin-left', $('#scene1 .small').css('margin-left'));
@@ -10,36 +12,34 @@ $(function() {
 
 
 	// keep track of current scene
-	var current_scene = 1; // technically it's 0, but this way initialization works
+	var current_scene = 0; // because first story section is scene1, and slideshow is a constant animation
 	// key track of last scroll location
 	var last_scroll = 0;
 
 	/*
 	 * The following events handle inter-scene movement via:
-	 * arrow click (down only)
 	 * scroll (up/down)
 	 * keydown (up/down)
 	 */
-	 // IMPORTANT: if I figure out a way to initialize animations based on scroll locations, it'll probably be worth disabling this
 	 // I could have it bounce up and down and maybe show a message like the current squarespace page does
-	$('#arrow svg').click(function() { 
-		// check that arrow is present
-		if($(this).css('opacity') >= 0.5) { // this can be subject to change
-			// fade out arrow
-			$(this).css('-webkit-animation', 'fadeOut 0.6s linear forwards');
-			// swoosh scene up // *** Maybe you just move down the page at a rate, and it doesn't actually go up
-			// $('#scene'+$(this).data('scene')).css('-webkit-animation', 'sceneSwooshUp 0.6s linear forwards');
-			// increment scene in arrow
-			$(this).data('scene', $(this).data('scene') + 1);
-			// increment scene
-			current_scene++;
-			console.log($(this).data('scene'));
-			console.log(current_scene);
-			// bring next scene in
+	// $('#arrow svg').click(function() { 
+	// 	// check that arrow is present
+	// 	if($(this).css('opacity') >= 0.5) { // this can be subject to change
+	// 		// fade out arrow
+	// 		$(this).css('-webkit-animation', 'fadeOut 0.6s linear forwards');
+	// 		// swoosh scene up // *** Maybe you just move down the page at a rate, and it doesn't actually go up
+	// 		// $('#scene'+$(this).data('scene')).css('-webkit-animation', 'sceneSwooshUp 0.6s linear forwards');
+	// 		// increment scene in arrow
+	// 		$(this).data('scene', $(this).data('scene') + 1);
+	// 		// increment scene
+	// 		current_scene++;
+	// 		console.log($(this).data('scene'));
+	// 		console.log(current_scene);
+	// 		// bring next scene in
 
-			// fade in arrow
-		}
-	});
+	// 		// fade in arrow
+	// 	}
+	// });
 
 	// function initializeScene(current_scene) {
 	// 	if (current_scene === 1) {
@@ -59,13 +59,15 @@ $(function() {
 	$(window).scroll(function(e) {
 		var current_scroll = $(window).scrollTop();
 		// fade out arrow before next section
-		if (current_scroll >= (current_scene-1) * $(window).height() + 20 && current_scroll <= (current_scene-1) * $(window).height() + 40) {
+		if (current_scroll >= current_scene * $(window).height() + 20 && current_scroll <= current_scene * $(window).height() + 40) {
 			$('#arrow svg').css('-webkit-animation', 'fadeOut 0.6s linear forwards');
 		} 
 		// Check if user is scrolling down
-		if (current_scroll > last_scroll) {
-			if (current_scroll >= current_scene * $(window).height() && current_scroll <= current_scene * $(window).height() + 20) {
-				
+		if (current_scroll > last_scroll) { // ** This will be the down arrow key logic. scrollTop should still work
+			// check if user scrolled into next scene
+			if (current_scroll >= (current_scene+1) * $(window).height() && current_scroll <= (current_scene+1) * $(window).height() + 20) {
+				// increment scene
+				current_scene++;
 				// initializeScene(current_scene); // don't abstract this because it may load after the handler (I could just put it in an earlier file)
 				if (current_scene === 1) {
 					$('#scene1-first').css('-webkit-animation', 'swooshUpFirst 0.55s linear forwards');
@@ -77,23 +79,22 @@ $(function() {
 					// hide previous scene content
 					$('#scene'+(current_scene-1)).children().css('opacity', '0');
 				}
-				// increment scene
-				current_scene++;
+				
 			}
-		} else {
+		} else { // ** This will be the up arrow key logic
 			// reinitialize animations
-			if (current_scene-1 === 1) { // currently on scene 1
+			if (current_scene === 1) { // currently on scene 1
 				console.log('hi');
 				// only bring arrow back
 				$('#arrow svg').css({'-webkit-animation': '', 'opacity': 1, 'top': '90vh'});
-				// check if animations need to reset
-				if (current_scroll <= (current_scene-2) * $(window).height() + 20) {
-					// decrement current scene
-					current_scene--;
-					// show scene's children again and remove animation
+				// check if animations need to reset (because in previous scene)
+				if (current_scroll <= (current_scene-1) * $(window).height() + 20) {
+					// show next scene's children again and remove animation
 					$('#scene'+current_scene).children().css({'opacity': '1', '-webkit-animation': ''});
 					// reset style ** NOTE: this is hardcoded right now...
 					$('#scene1-first').css('margin-top', '125vh');
+					// decrement current scene
+					current_scene--;
 				}
 			}
 		}
